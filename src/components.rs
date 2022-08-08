a169230bbd492a97ed4968c79f84fd56c531243a
l-Shane-l <shane.hamilton3@mail.dcu.ie>
2022-08-08 20:21:14 +0100

    added combat system

--- src/components.rs
+++ src/components.rs
@@ -2,8 +2,8 @@ pub use crate::prelude::*;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Render {
-    pub color : ColorPair,
-    pub glyph : FontCharType
+    pub color: ColorPair,
+    pub glyph: FontCharType,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -17,15 +17,21 @@ pub struct MovingRandomly;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToMove {
-    pub entity : Entity,
-    pub destination : Point
+    pub entity: Entity,
+    pub destination: Point,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Health {
     pub current: i32,
-    pub max: i32
+    pub max: i32,
 }
 
 #[derive(Clone, PartialEq)]
 pub struct Name(pub String);
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantsToAttack {
+    pub attacker: Entity,
+    pub victim: Entity,
+}
