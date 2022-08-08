4ec64290883819983458cd6840e2a656868f0af6
l-Shane-l <shane.hamilton3@mail.dcu.ie>
2022-08-08 18:56:25 +0100

    added hud

--- src/components.rs
+++ src/components.rs
@@ -5,6 +5,7 @@ pub struct Render {
     pub color : ColorPair,
     pub glyph : FontCharType
 }
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Player;
 
@@ -14,4 +15,17 @@ pub struct Enemy;
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct MovingRandomly;
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantsToMove {
+    pub entity : Entity,
+    pub destination : Point
+}
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Health {
+    pub current: i32,
+    pub max: i32
+}
+
+#[derive(Clone, PartialEq)]
+pub struct Name(pub String);
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -6,6 +6,9 @@ mod player_input;
 mod collisions;
 mod random_move;
 mod end_turn;
+mod movement;
+mod hud;
+mod tooltips;
 
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
@@ -13,15 +16,20 @@ pub fn build_input_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
+        .add_system(tooltips::tooltips_system())
         .build()
 }
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
@@ -30,10 +38,11 @@ pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
         .flush()
-        .add_system(collisions::collisions_system())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
