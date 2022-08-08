a5accbd17ee647c895ffa53d8ff5937c20c19a31
l-Shane-l <shane.hamilton3@mail.dcu.ie>
2022-08-08 17:35:54 +0100

    added turn based movement

--- src/components.rs
+++ src/components.rs
@@ -10,3 +10,8 @@ pub struct Player;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Enemy;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct MovingRandomly;
+
+
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -7,18 +7,21 @@ pub fn player_input(
     ecs: &mut SubWorld,
     #[resource] map: &Map,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera
+    #[resource] camera: &mut Camera,
+    #[resource] turn_state: &mut TurnState
 )
 {
+
     if let Some(key) = key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
-            VirtualKeyCode::Right => Point::new(1,0),
+            VirtualKeyCode::Right => Point::new(1, 0),
             VirtualKeyCode::Up => Point::new(0, -1),
-            VirtualKeyCode::Down => Point::new(0,1),
+            VirtualKeyCode::Down => Point::new(0, 1),
             _ => Point::new(0, 0),
         };
-        if delta.x != 0 || delta.y !=0 {
+
+        if delta.x != 0 || delta.y != 0 {
             let mut players = <&mut Point>::query()
                 .filter(component::<Player>());
             players.iter_mut(ecs).for_each(|pos| {
@@ -26,6 +29,7 @@ pub fn player_input(
                 if map.can_enter_tile(destination) {
                     *pos = destination;
                     camera.on_player_move(destination);
+                    *turn_state = TurnState::PlayerTurn;
                 }
             });
         }
