a9c6f87dab1813050a30dda2c6c6c7a95d7d8acd
l-Shane-l <shane.hamilton3@mail.dcu.ie>
2022-08-11 17:47:56 +0100

    added intelligence to monsters

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,40 +1,41 @@
 use crate::prelude::*;
 
-pub fn spawn_player(ecs : &mut World, pos : Point) {
-    ecs.push(
-        (Player, 
-            pos,
-            Render{
-                color: ColorPair::new(WHITE, BLACK),
-                glyph : to_cp437('@')
-            },
-            Health{ current: 20, max: 20 }
-        )
-    );
+pub fn spawn_player(ecs: &mut World, pos: Point) {
+    ecs.push((
+        Player,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('@'),
+        },
+        Health {
+            current: 10,
+            max: 10,
+        },
+    ));
 }
 
-pub fn spawn_monster(
-    ecs: &mut World, 
-    rng: &mut RandomNumberGenerator, 
-    pos : Point
-) {
-    let (hp, name, glyph) = match rng.roll_dice(1,10) { // (1)
-        1..=8 => goblin(),// (2)
-        _ => orc()
+pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    let (hp, name, glyph) = match rng.roll_dice(1, 10) {
+        // (1)
+        1..=8 => goblin(), // (2)
+        _ => orc(),
     };
 
-    ecs.push(
-        (Enemy,
-            pos,
-            Render{
-                color: ColorPair::new(WHITE, BLACK),
-                glyph,
-            },
-            MovingRandomly{},
-            Health{current: hp, max: hp},// (3)
-            Name(name)// (4)
-        )
-    );
+    ecs.push((
+        Enemy,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph,
+        },
+        ChasingPlayer {},
+        Health {
+            current: hp,
+            max: hp,
+        }, // (3)
+        Name(name), // (4)
+    ));
 }
 
 fn goblin() -> (i32, String, FontCharType) {
