e2053c86f504bec99eba59a31e30893a9e228cd3
l-Shane-l <shane.hamilton3@mail.dcu.ie>
2022-08-12 17:43:42 +0100

    added vision memory

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -3,8 +3,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(FieldOfView)]
 #[read_component(Player)]
-pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Camera) {
-    let mut fov = <&FieldOfView>::query().filter(component::<Player>()); // (1)
+pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera, ecs: &SubWorld) {
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
 
@@ -14,14 +14,30 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
         for x in camera.left_x..camera.right_x {
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
-            if map.in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
-                let idx = map_idx(x, y);
+            let idx = map_idx(x, y);
+            if map.in_bounds(pt)
+                && (player_fov.visible_tiles.contains(&pt) | map.revealed_tiles[idx])
+            {
+                // (1)
+                let tint = if player_fov.visible_tiles.contains(&pt) {
+                    // (2)
+                    WHITE
+                } else {
+                    DARK_GRAY
+                };
                 match map.tiles[idx] {
                     TileType::Floor => {
-                        draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), to_cp437('.'));
+                        draw_batch.set(
+                            pt - offset,
+                            ColorPair::new(
+                                tint, // (3)
+                                BLACK,
+                            ),
+                            to_cp437('.'),
+                        );
                     }
                     TileType::Wall => {
-                        draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), to_cp437('#'));
+                        draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), to_cp437('#'));
                     }
                 }
             }
