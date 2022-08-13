bba4d15ffa74cca7d4836c21fc0a258904718abd
l-Shane-l <shane.hamilton3@mail.dcu.ie>
2022-08-13 23:54:21 +0100

    second push on first flight, added cellular automata

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,10 +1,12 @@
 use crate::prelude::*;
 mod empty;
-use empty::EmptyArchitect;
+//mod rooms;
+//use rooms::RoomsArchitect;
+mod automata;
+use automata::CellularAutomataArchitect;
 
 trait MapArchitect {
-    // (1)
-    fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder; // (2)
+    fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
 }
 
 const NUM_ROOMS: usize = 20;
@@ -18,7 +20,7 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = EmptyArchitect {};
+        let mut architect = CellularAutomataArchitect {};
         architect.new(rng)
     }
 
@@ -109,5 +111,32 @@ impl MapBuilder {
                 self.apply_horizontal_tunnel(prev.x, new.x, new.y);
             }
         }
+    }
+
+    fn spawn_monsters(&self, start: &Point, rng: &mut RandomNumberGenerator) -> Vec<Point> {
+        const NUM_MONSTERS: usize = 50;
+        let mut spawnable_tiles: Vec<Point> = self
+            .map
+            .tiles
+            .iter()
+            .enumerate()
+            .filter(|(idx, t)|// (1)
+                **t == TileType::Floor &&
+                    DistanceAlg::Pythagoras.distance2d(
+                        *start,
+                        self.map.index_to_point2d(*idx)
+                    ) > 10.0)
+            .map(|(idx, _)| self.map.index_to_point2d(idx))
+            .collect();
+
+        let mut spawns = Vec::new();
+        for _ in 0..NUM_MONSTERS {
+            let target_index = rng
+                .random_slice_index(&spawnable_tiles) // (2)
+                .unwrap();
+            spawns.push(spawnable_tiles[target_index].clone());
+            spawnable_tiles.remove(target_index); // (3)
+        }
+        spawns
     }
 }
